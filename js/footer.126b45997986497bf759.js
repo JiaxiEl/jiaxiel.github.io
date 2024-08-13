2600ed2f8df73528bbcedc5ef001e2fe6e2def27
ChrisChen <jiaxic7@uci.edu>
2024-08-12 17:02:46 -0700

    Updates

--- js/footer.126b45997986497bf759.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("DOMContentLoaded",(function(){fetch("footer.html").then((function(t){return t.text()})).then((function(t){var e=document.getElementById("footer-placeholder");e&&(e.innerHTML=t)})).catch((function(t){}))}));
